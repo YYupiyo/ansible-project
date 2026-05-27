b1f3e359716f42408c849d2ec791199eb5fbc029
YYupiyo <dkalasnikova8@gmail.com>
2026-05-27 08:08:14 +0000

    Add handlers and block/rescue/always for error handling

--- roles/role_app/files/app.py
+++ roles/role_app/files/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 import socket
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     hostname = socket.gethostname()
-    return f"Hello from {hostname} in {{ env }} environment!"
+    env = os.getenv('ENV', 'unknown')
+    return f"Hello from {hostname} in {env} environment!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
